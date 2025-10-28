89fa6bc0bf03b05459976f7ccc8e0c192992f532
Artem-1-1 <aleksejchukartem@gmail.com>
2025-10-28 11:08:30 +0300

    Exercises 13/14/15/16 Completed.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-};
+    const clearString = str
+      .toLowerCase()
+      .split('')
+      .filter((character) => alphanumerical.includes(character))
+      .join('');
+
+    const reverseStr = clearString.split('').reverse().join('');
+    
+    return clearString === reverseStr;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let count;
+    if (typeof n !== 'number') {
+        count = parseInt(n);
+    }
+    else {
+        count = n;
+    }
+    
+    if (count < 0) return 'OOPS';
+    if (count === 0) return 0;
 
+    let previousNum = 1;
+    let nextNum = 0;
+
+    for (i = 2;i <= count; i++) {
+        let current = previousNum + nextNum;
+        nextNum =  previousNum;
+        previousNum = current;
+    }
+    return previousNum;
 };
 
 // Do not edit below this line
